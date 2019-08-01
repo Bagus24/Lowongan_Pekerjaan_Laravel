d9ed63eb1fe2e30f007020e54724c2f6c37484f9
Bagus24 <firgiawanbagus@gmail.com>
2019-08-01 21:53:16 +0700

    update register

--- private/resources/views/auth/register.blade.php
+++ private/resources/views/auth/register.blade.php
@@ -1,81 +1,3 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
-
 <!DOCTYPE html>
 	<html lang="zxx" class="no-js">
 
@@ -95,31 +17,37 @@
                                 </div>
                             </div>
                             
-                            <form class="form-area" method="POST" action="{{ route('login') }}" class="contact-form text-right">
+                            <form class="form-area" method="POST" action="{{ route('register') }}" class="contact-form text-right">
                             @csrf    
                             <div class="row">	
 									<div class="col-lg-12 form-group">
                                             @error('email')
-                                                <div class="alert alert-danger" role="alert">Email atau password salah</div>
+                                                <div class="alert alert-danger" role="alert">Email sudah terdaftar</div>
                                             @enderror
                                             @error('password')
-                                                <div class="alert alert-danger" role="alert">Email atau password salah</div>
+                                                <div class="alert alert-danger" role="alert">Kata sandi tidak cocok atau kurang dari 8 karakter</div>
                                             @enderror
-										<input name="email" value="{{ old('email') }}" placeholder="Masukan email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Masukan email'" class="common-input mb-20 form-control @error('email') is-invalid @enderror" required="" type="email">
+										<input pattern="[A-Za-z ]+"  id="name" type="text" placeholder="Masukan nama lengkap" class="common-input mb-20 form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                            @error('name')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+										<input id="email" type="email" placeholder="Masukan email" class="common-input mb-20 form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
                                             
-										<input name="password" placeholder="Masukan password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Masukan password'" class="common-input mb-20 form-control @error('password') is-invalid @enderror" required="" type="password">
+                                        <input id="password" type="password" placeholder="Masukan kata sandi" class="common-input mb-20 form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
                                             
-                                        <div class="switch-wrap d-flex">
-                                            
-                                            <div class="primary-checkbox">
-                                                <input type="checkbox" name="remember" id="default-checkbox" {{ old('remember') ? 'checked' : '' }}>
-                                                <label for="default-checkbox"></label>
-                                            </div>
-                                            <p class="text-white">&nbsp;Ingat saya</p>
-                                        </div>
+										<input id="password-confirm" type="password"  placeholder="Masukan ulang kata sandi"  class="common-input mb-20 form-control" name="password_confirmation" required autocomplete="new-password" >
+                                        
 
-                                        <button type="submit" class="genric-btn primary" style="float: center;">Masuk</button>
-										<div class="mt-20 alert-msg" style="text-align: left;"></div>
+                                        <div class="row justify-content-center form-wrap">
+                                            <button type="submit" class="genric-btn primary" style="float: center;">Daftar</button>
+
+                                            <p class="text-white">&nbsp;&nbsp;Sudah punya akun? <a href="{{ route('login') }}"> <strong>Masuk</strong> </a></p>
+                                                
+                                        </div>
+                                        
+                                        
 									</div>
 								</div>
 							</form>	
@@ -134,7 +62,6 @@
 			
 		</body>
 	</html>
-
 
 
 
